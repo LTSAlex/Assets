0e726394a96572bde8aafb3f9a70423eb8263f8e
LTSAlex <103276530+LTSAlex@users.noreply.github.com>
2023-11-18 20:24:15 -0500

    Update Assets.lua

--- Assets.lua
+++ Assets.lua
@@ -30,13 +30,29 @@ local NinjaWalk = "http://www.roblox.com/asset/?id=10921162768"
 local NinjaJump = "http://www.roblox.com/asset/?id=10921160088"
 local NinjaFall = "http://www.roblox.com/asset/?id=10921159222"
 
--- Borock Assets--
+--Borock Assets--
 local BorockIdle1 = "http://www.roblox.com/asset/?id=3303162274"
 local BorockIdle2 = "http://www.roblox.com/asset/?id=3303162549"
 local BorockRun = "http://www.roblox.com/asset/?id=3236836670"
 local BorockWalk = "http://www.roblox.com/asset/?id=3303162967"
 local BorockJump = "http://www.roblox.com/asset/?id=10921263860"
 local BorockFall =  "http://www.roblox.com/asset/?id=10921262864"
+
+--Werewolf Assets--
+local WerewolfIdle1 = "http://www.roblox.com/asset/?id=10921330408"
+local WerewolfIdle2 = "http://www.roblox.com/asset/?id=10921333667"
+local WerewolfRun = "http://www.roblox.com/asset/?id=10921336997"
+local WerewolfWalk = "http://www.roblox.com/asset/?id=10921342074"
+local WerewolfJump = "http://www.roblox.com/asset/?id=1083218792"
+local WerewolfFall = "http://www.roblox.com/asset/?id=10921337907"
+
+--Vampire Assets--
+local VampireIdle1 = "http://www.roblox.com/asset/?id=10921315373"
+local VampireIdle2 = "http://www.roblox.com/asset/?id=10921316709"
+local VampireRun = "http://www.roblox.com/asset/?id=10921320299"
+local VampireWalk = "http://www.roblox.com/asset/?id=10921326949"
+local VampireJump = "http://www.roblox.com/asset/?id=10921322186"
+local VampireFall = "http://www.roblox.com/asset/?id=10921321317"
 
 local userInputService = game:GetService("UserInputService")
 
@@ -54,8 +70,8 @@ userInputService.InputBegan:Connect(function(input, gameProcessedEvent)
 			Animate.fall.FallAnim.AnimationId = NinjaFall
 			game.Players.LocalPlayer.Character.Humanoid.Jump = false
 			wait(0.2)	
-			Animate.idle.Animation1.AnimationId = ToyIdle1
-			Animate.idle.Animation2.AnimationId = ToyIdle2
+			Animate.idle.Animation1.AnimationId = VampireIdle1
+			Animate.idle.Animation2.AnimationId = VampireIdle2
 		end
 	end
 end)
